6ec9753149eea3325156711c8bb560e9ce7782b3
Yeohyungoo <hyungu0611@gmail.com>
2019-04-16 18:41:01 +0900

    0416

--- INSTA/mton/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MtonConfig(AppConfig):
-    name = 'mton'
--- INSTA/onetoone/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OnetooneConfig(AppConfig):
-    name = 'onetoone'
