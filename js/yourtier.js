2bf34f51b1eb4050086e14d90a03a74fa4e74ff4
여현구(Yeo <a202002008@A202002008-01.local>
2020-10-04 14:40:54 +0900

    fix

--- js/yourtier.js
+++ js/yourtier.js
@@ -1,6 +1,6 @@
 const championList = ['Aatrox', 'Ahri', 'Akali', 'Alistar', 'Amumu', 'Anivia', 'Annie', 'Aphelios', 'Ashe', 'AurelionSol', 'Azir', 'Bard', 'Blitzcrank', 'Brand', 'Braum', 'Caitlyn', 'Camille', 'Cassiopeia', 'Chogath', 'Corki', 'Darius', 'Diana', 'DrMundo', 'Draven', 'Ekko', 'Elise', 'Evelynn', 'Ezreal', 'Fiddlesticks', 'Fiora', 'Fizz', 'Galio', 'Gangplank', 'Garen', 'Gnar', 'Gragas', 'Graves', 'Hecarim', 'Heimerdinger', 'Illaoi', 'Irelia', 'Ivern', 'Janna', 'JarvanIV', 'Jax', 'Jayce', 'Jhin', 'Jinx', 'Kaisa', 'Kalista', 'Karma', 'Karthus', 'Kassadin', 'Katarina', 'Kayle', 'Kayn', 'Kennen', 'Khazix', 'Kindred', 'Kled', 'Kogmaw', 'Leblanc', 'LeeSin', 'Leona', 'Lillia', 'Lissandra', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar', 'Maokai', 'MasterYi', 'MissFortune', 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus', 'Neeko', 'Nidalee', 'Nocturne', 'Nunu', 'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Pyke', 'Qiyana', 'Quinn', 'Rakan', 'Rammus', 'Reksai', 'Renekton', 'Rengar', 'Riven', 'Rumble', 'Ryze','Samira', 'Sejuani', 'Senna', 'Sett', 'Shaco', 'Shen', 'Shyvana', 'Singed', 'Sion', 'Sivir', 'Skarner', 'Sona', 'Soraka', 'Swain', 'Sylas', 'Syndra', 'TahmKench', 'Taliyah', 'Talon', 'Taric', 'Teemo', 'Thresh', 'Tristana', 'Trundle', 'Tryndamere', 'TwistedFate', 'Twitch', 'Udyr', 'Urgot', 'Varus', 'Vayne', 'Veigar', 'Velkoz', 'Vi', 'Viktor', 'Vladimir', 'Volibear', 'Warwick', 'Wukong', 'Xayah', 'Xerath', 'XinZhao', 'Yasuo', 'Yone', 'Yorick', 'Yuumi', 'Zac', 'Zed', 'Ziggs', 'Zilean', 'Zoe', 'Zyra']
 
-const positionList = ['top', 'jg', 'mid', 'bot', 'sp']
+const positionList = ['top', 'jg', 'mid', 'bot', 'sp', 'top1', 'jg1', 'mid1', 'bot1', 'sp1']
 
 for (var a = 0; a < positionList.length; a++) {
     for(var count = 0; count < championList.length; count++){                
@@ -9,11 +9,17 @@ for (var a = 0; a < positionList.length; a++) {
     }
 }
 
-
 $("#topSelect").change(function() {
     $("#topSelect option:selected").each(function() {
         console.log($("#topSelect").val())
         $("#topChampImg").attr("src", "http://ddragon.leagueoflegends.com/cdn/10.20.1/img/champion/"+$("#topSelect").val()+".png");
+    });
+});
+
+$("#top1Select").change(function() {
+    $("#top1Select option:selected").each(function() {
+        console.log($("#top1Select").val())
+        $("#top1ChampImg").attr("src", "http://ddragon.leagueoflegends.com/cdn/10.20.1/img/champion/"+$("#top1Select").val()+".png");
     });
 });
 
@@ -45,6 +51,34 @@ $("#spSelect").change(function() {
     });
 });
 
+$("#jg1Select").change(function() {
+    $("#jg1Select option:selected").each(function() {
+        console.log($("#jg1Select").val())
+        $("#jg1ChampImg").attr("src", "http://ddragon.leagueoflegends.com/cdn/10.20.1/img/champion/"+$("#jg1Select").val()+".png");
+    });
+});
+
+$("#mid1Select").change(function() {
+    $("#mid1Select option:selected").each(function() {
+        console.log($("#mid1Select").val())
+        $("#mid1ChampImg").attr("src", "http://ddragon.leagueoflegends.com/cdn/10.20.1/img/champion/"+$("#mid1Select").val()+".png");
+    });
+});
+
+$("#bot1Select").change(function() {
+    $("#bot1Select option:selected").each(function() {
+        console.log($("#bot1Select").val())
+        $("#bot1ChampImg").attr("src", "http://ddragon.leagueoflegends.com/cdn/10.20.1/img/champion/"+$("#bot1Select").val()+".png");
+    });
+});
+
+$("#sp1Select").change(function() {
+    $("#sp1Select option:selected").each(function() {
+        console.log($("#sp1Select").val())
+        $("#sp1ChampImg").attr("src", "http://ddragon.leagueoflegends.com/cdn/10.20.1/img/champion/"+$("#sp1Select").val()+".png");
+    });
+});
+
 
 
 
@@ -59,6 +93,8 @@ function Click(e) {
         this.className = 'score1'
     } else if (this.className == 'score1') {
         this.className = 'score2'
+    } else if (this.className == 'score2') {
+        this.className = 'score3'
     } else {
         this.className = 'score'
     }
